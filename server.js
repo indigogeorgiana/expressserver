118283f71bd2547ed9160edabc22f6b6b0d8806b
Indigo Phillips <indigogeorgiana@gmail.com>
2018-06-01 11:26:56 +1200

    completed Respond with string

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const server = express()
 
+const port = 300
+
+server.get('/compliment', (req, res) => {
+  res.send('You have cool hair')
+})
+
 module.exports = server
