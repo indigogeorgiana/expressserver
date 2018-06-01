f41eaea6f0ded1638a345e1dfc02f41e7e0e420e
Indigo Phillips <indigogeorgiana@gmail.com>
2018-06-01 13:26:46 +1200

    Completed Enable a folder of static files

--- server.js
+++ server.js
@@ -4,20 +4,30 @@ const express = require('express')
 const server = express()
 
 server.use(express.urlencoded({extended: false}))
+server.use(express.static('public'))
 
 server.get('/compliment', (req, res) => {
   res.send('You have cool hair')
 })
 
-// server.get('/profile', (req, res) => {
-// res.sendFile(path.join(__dirname, 'silvia.html'))
-// })
+server.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'silvia.html'))
+})
 
 server.get('/profile', (req, res) => {
   const name = req.query.name
   if (name === 'silvia') {
     res.sendFile(path.join(__dirname, 'silvia.html'))
   } else if (name === 'sampson') {
+    res.sendFile(path.join(__dirname, 'sampson.html'))
+  }
+})
+
+server.get('/profiles/:id', (req, res) => {
+  const id = req.params.id
+  if (id === '1') {
+    res.sendFile(path.join(__dirname, 'silvia.html'))
+  } else if (id === '2') {
     res.sendFile(path.join(__dirname, 'sampson.html'))
   }
 })
