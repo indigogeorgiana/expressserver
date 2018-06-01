a016227052922c636740732756248afb2e5dd2a5
Indigo Phillips <indigogeorgiana@gmail.com>
2018-06-01 12:21:47 +1200

    completed Respond based on the query

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.urlencoded({extended: false}))
+
 server.get('/compliment', (req, res) => {
   res.send('You have cool hair')
 })
 
+// server.get('/profile', (req, res) => {
+// res.sendFile(path.join(__dirname, 'silvia.html'))
+// })
+
 server.get('/profile', (req, res) => {
-  res.sendFile(path.join(__dirname, 'silvia.html'))
+  const name = req.query.name
+  if (name === 'silvia') {
+    res.sendFile(path.join(__dirname, 'silvia.html'))
+  } else if (name === 'sampson') {
+    res.sendFile(path.join(__dirname, 'sampson.html'))
+  }
 })
 
 module.exports = server
